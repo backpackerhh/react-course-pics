882229dc29ef8e9f61b8d0181da724d245450912
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-16 16:29:15 +0100

    Allow to store search term in component state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 export default class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label htmlFor="search">Image Search</label>
-            <input type="text" id="search" />
+            <input
+              type="text"
+              id="search"
+              onChange={this.onChange}
+              value={this.state.term}
+            />
           </div>
         </form>
       </div>
