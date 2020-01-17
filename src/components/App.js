37b607448fc27f2ea93ad815abb7d4eb072cc250
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-17 10:02:46 +0100

    Allow to set state with search results after async requests

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,17 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 export default class App extends React.Component {
-  onSearchSubmit = term => {
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
     const UNSPLASH_SEARCH_PHOTOS_ENDPOINT =
       "https://api.unsplash.com/search/photos";
     const UNSPLASH_ACCESS_KEY =
       "aee528a67eadbc53d523c43aadd069177714fb17516dd8c176a69cbf27054b99";
 
-    axios.get(UNSPLASH_SEARCH_PHOTOS_ENDPOINT, {
+    const response = await axios.get(UNSPLASH_SEARCH_PHOTOS_ENDPOINT, {
       params: {
         query: term
       },
@@ -18,12 +22,15 @@ export default class App extends React.Component {
         Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
       }
     });
+
+    this.setState({ images: response.data.results });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
