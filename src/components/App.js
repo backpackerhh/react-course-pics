51a325a159a1fc51d6237a6d91d82c8a5029e8f6
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-17 09:40:07 +0100

    Allow to make request with axios to Unsplash API
    More info:
    https://unsplash.com/documentation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 
 export default class App extends React.Component {
   onSearchSubmit = term => {
-    console.log(term);
+    const UNSPLASH_SEARCH_PHOTOS_ENDPOINT =
+      "https://api.unsplash.com/search/photos";
+    const UNSPLASH_ACCESS_KEY =
+      "aee528a67eadbc53d523c43aadd069177714fb17516dd8c176a69cbf27054b99";
+
+    axios.get(UNSPLASH_SEARCH_PHOTOS_ENDPOINT, {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
+      }
+    });
   };
 
   render() {
